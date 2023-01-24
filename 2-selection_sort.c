a316de8d66067f38dd9c950106d0563014215cea
trevordiex <trevordiex@gmail.com>
2023-01-24 17:47:18 +0100

    fixing selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,6 +6,9 @@
 * @size: the size of the array
 *
 * Description: array contains only int values
+* iterates over the array starting from current index
+* selects the min element greater than or equal array[i]
+* swaps array[i] with element with minimum element
 */
 void selection_sort(int *array, size_t size)
 {
@@ -19,13 +22,17 @@ void selection_sort(int *array, size_t size)
 		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-		if (array[j] < array[min_index])
-			min_index = j;
+			if (array[j] < array[min_index])
+				min_index = j;
 		}
-		temp = array[i];
-		array[i] = array[min_index];
-		array[min_index] = temp;
 
-		print_array(array, size);
+		if (i != min_index)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+
+			print_array(array, size);
+		}
 	}
 }
